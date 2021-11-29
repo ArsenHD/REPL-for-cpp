c85f66186fab6d8256fe27b0957125de62b25ccb
Arsen Nagdalian <nagoarsen@mail.ru>
2021-11-30 01:27:54 +0300

    Implement source generation + library build

--- main.cpp
+++ main.cpp
@@ -5,32 +5,80 @@
 #include <dlfcn.h>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <vector>
 
 static const std::string LIB = "lib";
 static const std::string EXT = ".so";
 
-std::string get_lib_name(int id) {
+static const std::string SOURCE = "SOURCE";
+static const std::string CPP = ".cpp";
+
+static const char *METHOD_NAME = "foo";
+
+static const std::string TAB = "    ";
+
+void print_tabulation(std::ofstream &file, int amount) {
+    for (int i = 0; i < amount; i++) {
+        file << TAB;
+    }
+}
+
+std::string get_source_file_name(int id) {
+    return SOURCE + std::to_string(id) + CPP;
+}
+
+std::string get_library_name(int id) {
     return LIB + std::to_string(id) + EXT;
 }
 
 void load_and_run(int id) {
-    std::string lib_name = get_lib_name(id);
+    std::string lib_name = get_library_name(id);
     void *so = dlopen(lib_name.c_str(), RTLD_LAZY | RTLD_GLOBAL);
-    auto (*f)() = reinterpret_cast<void (*)()>(dlsym(so, lib_name.c_str()));
+    auto (*f)() = reinterpret_cast<void (*)()>(dlsym(so, METHOD_NAME));
     f();
 }
 
-int main(int argc, char **argv) {
-    std::string line;
-    int id = 0;
-    while (std::cin >> line) {
-        std::string lib_name = get_lib_name(id);
-        std::cout << lib_name << std::endl;
-        std::ofstream file(lib_name);
-        file.open(lib_name);
+void generate_source_file(const std::vector <std::string> &lines, std::ofstream &file) {
+    // TODO: add header includes to the beginning of the file
+    std::string function_header = "void " + SOURCE + "() {";
+    file << function_header << std::endl;
+    for (const std::string &line : lines) {
+        print_tabulation(file, 1);
         file << line;
-        id++;
-        file.close();
+        file << std::endl;
     }
+    file << "}";
+    file << std::endl;
+}
+
+void build_library(const std::string &source, const std::string &library_name) {
+    std::string command = "g++ -fpic -shared -o " + library_name + " " + source;
+    std::system(command.c_str());
+}
+
+int main(int argc, char **argv) {
+    std::string current_line;
+    int id = 0;
+
+    std::vector <std::string> lines;
+
+    while (std::cin >> current_line) {
+        std::string lib_name = get_library_name(id);
+        std::string source_file_name = get_source_file_name(id);
+
+        std::ofstream source_file(source_file_name);
+        source_file.open(lib_name);
+
+        lines.push_back(current_line);
+
+        generate_source_file(lines, source_file);
+
+        source_file.close();
+
+        build_library(source_file_name, lib_name);
+        id++;
+    }
+
     return 0;
 }
