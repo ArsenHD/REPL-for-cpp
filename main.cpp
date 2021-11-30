df468ba1149a9cc096001e060412f0efe0e9ee7b
Cro3SwI2mer <Cro3SwI2mer@github.com>
2021-11-30 16:05:04 +0300

    Implement dynamic library loading and call

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 // Created by kirill on 29.11.2021.
 //
 
-#include <assert.h>
 #include <dlfcn.h>
 #include <iostream>
 #include <fstream>
@@ -10,14 +9,25 @@
 static const std::string LIB = "lib";
 static const std::string EXT = ".so";
 
+std::string get_lib_name(int id) {
+    return LIB + std::to_string(id) + EXT;
+}
+
+void load_and_run(int id) {
+    std::string lib_name = get_lib_name(id);
+    void *so = dlopen(lib_name.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+    auto (*f)() = reinterpret_cast<void (*)()>(dlsym(so, lib_name.c_str()));
+    f();
+}
+
 int main(int argc, char **argv) {
     std::string line;
     int id = 0;
     while (std::cin >> line) {
-        std::string file_name = LIB + std::to_string(id) + EXT;
-        std::cout << file_name << std::endl;
-        std::ofstream file(file_name);
-        file.open(file_name);
+        std::string lib_name = get_lib_name(id);
+        std::cout << lib_name << std::endl;
+        std::ofstream file(lib_name);
+        file.open(lib_name);
         file << line;
         id++;
         file.close();
