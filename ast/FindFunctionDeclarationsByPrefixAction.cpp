8fc814ea2491b87b1e4c4103064b85d6bdb8fa94
Kamenev <kamenev.yury@huawei.com>
2021-12-01 18:05:19 +0300

    Moved signatures extracting classes to separate directory

--- ast/FindFunctionDeclarationsByPrefixAction.cpp
+++
@@ -1,9 +0,0 @@
-#include "FindFunctionDeclarationsByPrefixAction.hpp"
-#include "FindFunctionDeclarationsByPrefixConsumer.hpp"
-
-using namespace ast_tools;
-
-std::unique_ptr<clang::ASTConsumer> FindFunctionDeclarationsByPrefixAction::CreateASTConsumer(
-        clang::CompilerInstance& Compiler, llvm::StringRef InFile) {
-    return std::make_unique<FindFunctionDeclarationsByPrefixConsumer>(&Compiler.getASTContext(), prefix_, signatures_);
-}
--- ast/FindFunctionDeclarationsByPrefixConsumer.cpp
+++
@@ -1,7 +0,0 @@
-#include "FindFunctionDeclarationsByPrefixConsumer.hpp"
-
-using namespace ast_tools;
-
-void FindFunctionDeclarationsByPrefixConsumer::HandleTranslationUnit(clang::ASTContext& Context) {
-    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-}
