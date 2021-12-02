d0b59674548742e278681d849f5df7311ab0cb1d
Cro3SwI2mer <Cro3SwI2mer@github.com>
2021-12-02 17:46:53 +0300

    Support different compilers

--- utils/library_utils.cpp
+++ utils/library_utils.cpp
@@ -1,11 +1,18 @@
 #include "library_utils.hpp"
 
+const std::string cxx =
+#ifdef __clang__
+        "clang++";
+#else
+        "g++";
+#endif
+
 std::string LibraryUtils::get_library_name(int id) {
     return "gen/libs/" + LIB + std::to_string(id) + EXT;
 }
 
 int LibraryUtils::build_library(const std::string &source, const std::string &library_new) {
-    std::string command = "g++ -fPIC -shared -o " + library_new + " " + source;
+    std::string command = cxx + " -fPIC -shared -o " + library_new + " " + source;
     int out = std::system(command.c_str());
     return out;
 }
